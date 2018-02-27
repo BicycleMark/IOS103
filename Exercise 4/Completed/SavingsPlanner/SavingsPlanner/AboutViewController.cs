3f658c5f19a92371b538a084db83c427fc158a6d
Adrian <adrian.stevens@gmail.com>
2018-02-27 09:14:52 -0800

    initial commit

---
+++ Exercise 4/Completed/SavingsPlanner/SavingsPlanner/AboutViewController.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace SavingsPlanner
+{
+    public partial class AboutViewController : UIViewController
+    {
+        public AboutViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        partial void CloseButton_TouchUpInside(UIButton sender)
+        {
+            DismissViewController(true, null);
+        }
+    }
+}
